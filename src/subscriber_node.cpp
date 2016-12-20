b417d83433cf6cffc10dd15bb2b471d9197d34c3
raggyftw <hansenoliie@gmail.com>
2016-12-20 11:20:30 +0100

    Update subscriber_node.cpp

--- src/subscriber_node.cpp
+++ src/subscriber_node.cpp
@@ -3,24 +3,23 @@
 #include <std_msgs/Int8.h>
 #include "sound_play/sound_play.h"
 
+std::string path_to_sounds;
 // Declaring a constructor
 class sound{
 private:
   // Declaring a soundclient for use in the constructor
   sound_play::SoundClient sc;
-  std::string path_to_sounds;
-
 public:
   // Creating the callback function
   void Callback(const std_msgs::Int8::ConstPtr& msg) {
     // Setting the desired frequency
-    ros::Rate loop_rate(0.5);
+    ros::Rate loop_rate(1);
     // Initiating a switch statement
     switch (msg ->data) {
       case 1:
         // Use soundclient to play certain sound
         sc.playWave(path_to_sounds+"G.wav");
-        break; 
+        break;
       case 2:
         sc.playWave(path_to_sounds+"A.wav");
         break;
@@ -45,19 +44,18 @@ public:
 int main (int argc, char* argv[])
 {
   // Here the publising queue for the publisher is set to 1.
-  static const uint32_t ROS_QUEUE_PUB = 1;
-  
+  static const uint32_t ROS_QUEUE_SUB = 1;
+
   // initializing ros and naming the node.
   ros::init(argc, argv, "subscriber_node");
   // Creating nodehandler.
   ros::NodeHandle n;
-  
+
   sound music; // Storing the sound class information in music.
-  // Defining the absolute path to sound files
-  std::path_to_sounds = "/home/oliver/ros_ws/src/guidebot/piano/";
   // Subscribing to the topic depth_value and return the callback from the constructor sound
   ros::Subscriber sub = n.subscribe("depth_value", ROS_QUEUE_SUB, &sound::Callback, &music);
-  
+  // Defining the absolute path to sound files
+  path_to_sounds = "/home/oliver/ros_ws/src/pianobot/piano/";
   ros::spin();
   return 0;
-} // End of int main. 
+} // End of int main.
